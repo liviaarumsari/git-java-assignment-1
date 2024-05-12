6a6cd1ceea8d9c5d9cadf88dc3dd3cb3c2fd7c39
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-12 17:50:21 +0700

    feat: add latihan 2

---
+++ latihan_2.java
@@ -0,0 +1,24 @@
+public class latihan_2 {
+    public static void main(String[] args) {
+        // Buat variabel
+        int a = 12;
+        int b = 8;
+        int c = 5;
+        // Hitung
+        int hasill = a + b - c;
+        int hasil2 = a * b / c;
+        int hasil3 = a + b * c;
+        int hasil4 = a + b / c;
+        int hasil5 = (a + b)*c;
+        int hasil6 = (a - b)*c;
+        // Print
+        System.out.println(hasill);
+        System.out.println(hasil2);
+        System.out.println(hasil3);
+        System.out.println(hasil4);
+        System.out.println(hasil5);
+        System.out.println(hasil6);
+
+        // ANALISA: seluruh baris kode telah berjalan dengan lancara sehingga tidak perlu perbaikan
+    }
+}
