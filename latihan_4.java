65e0454b2492fd73302d4e4992b37cfbb6bbe05e
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-12 18:07:19 +0700

    feat: add latihan 4

---
+++ latihan_4.java
@@ -0,0 +1,21 @@
+public class latihan_4 {
+    public static void main(String[] args) {
+        // Buat variabel
+        int a = 10;
+        int b = 8;
+        int c = 12;
+        int d = 5;
+
+        // Output tes
+        System.out.println("Tes ke 1 = " + Boolean.toString(a > b));
+        System.out.println("Tes ke 2 = " + Boolean.toString(d < c));
+        System.out.println("Tes ke 3 = " + Boolean.toString(a >= b));
+        System.out.println("Tes ke 4 = " + Boolean.toString(d <= c));
+        System.out.println("Tes ke 5 = " + Boolean.toString((a - 2) == b));
+        System.out.println("Tes ke 6 = " + Boolean.toString(a != b));
+        System.out.println("Tes ke 7 = " + Boolean.toString(a > c));
+        System.out.println("Tes ke 8 = " + Boolean.toString(a <= b));
+        System.out.println("Tes ke 9 = " + Boolean.toString(a == b));
+        System.out.println("Tes ke 10 = " + Boolean.toString(c - 7 != d));
+    }
+}
