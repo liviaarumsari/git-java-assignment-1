004769bf86323c96792374bbba4e307a1e770c8e
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-12 17:58:14 +0700

    feat: add latihan 3

---
+++ latihan_3.java
@@ -0,0 +1,36 @@
+public class latihan_3 {
+    public static void main(String[] args) {
+        // Buat variabel awal
+        int a = 0;
+        int b = 0;
+        int c = 9;
+        int d = 9;
+        // Print ke 1
+        System.out.println("Print ke 1");
+        System.out.println("A = " + a++); // hasil : 0
+        System.out.println("B = " + ++b); // hasil : 1
+        System.out.println("C = " + c--); // hasil : 9
+        System.out.println("D = " + --d); // hasit : 8
+
+        // Print ke 2
+        System.out.println("Print ke 2");
+        System.out.println("A = " + a++); // hasil : 1
+        System.out.println("B = " + ++b); // hasil : 2
+        System.out.println("C = " + c--); // hasil : 8
+        System.out.println("D = " + --d); // hasit : 7
+
+        // Print ke 3
+        System.out.println("Print ke 3");
+        System.out.println("A = " + a++); // hasil : 2
+        System.out.println("B = " + ++b); // hasil : 3
+        System.out.println("C = " + c--); // hasil : 7
+        System.out.println("D = " + --d); // hasit : 6
+
+        // Print ke 4
+        System.out.println("Print ke 4");
+        System.out.println("A = " + a); // hasil : 3
+        System.out.println("B = " + ++b); // hasil : 4
+        System.out.println("C = " + c); // hasil : 6
+        System.out.println("D = " + --d); // hasit : 5
+    }
+}
